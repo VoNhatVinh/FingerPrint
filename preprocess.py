0a1b992c156f52ee5b54d4c28a7203d23f7636e8
VoNhatVinh <yenlang2017@gmail.com>
2019-05-19 11:45:27 +0700

    done nhé

--- preprocess.py
+++ preprocess.py
@@ -8,6 +8,8 @@ from binarization import binary_image
 from thining_new import thinning
 from minutiae import minutiae_extraction_filter
 
+
+
 '''
 Preprocess image include: grayscale, normalize, gabor_filter, binarization, minutiae
 
@@ -21,19 +23,19 @@ Return
 preprocessed imaged 
 '''
 def preprocess(img_path):
-    print("Preprocess in image:", img_path)
-    a = cv2.imread(img_path)
-    gray = grayscale(a)
-    normal = normalize(gray, 50, 300)
-    direct = orientation_estimation(normal)
-    gabor = gabor_filter(normal, direct, 4, 1/7, 3)
-    binary = binary_image(gabor, 50)
-    thinned = thinning(binary)
-    list_minutiae = minutiae_extraction_filter(thinned)
+  print("Preprocess in image: ", img_path)
+  a = cv2.imread(img_path)
+  gray = grayscale(a)
+  normal = normalize(gray, 50, 300)
+  direct = orientation_estimation(normal)
+  gabor = gabor_filter(normal, direct, 4, 1/7, 3)
+  binary = binary_image(gabor, 50)
+  thinned = thinning(binary)
+  list_minutiae = minutiae_extraction_filter(thinned)
     
-	w, h = gray.shape
-    minutiae_img = np.zeros((w, h))    
-	for i in range(len(list_minutiae)):
-        minutiae_img[list_minutiae[i][0], list_minutiae[i][1]] = 1 #the minutiae point = 1, the others = 0
+  w, h = gray.shape
+  minutiae_img = np.zeros((w, h))    
+  for i in range(len(list_minutiae)):
+    minutiae_img[list_minutiae[i][0], list_minutiae[i][1]] = 1 #the minutiae point = 1, the others = 0
 
-    return minutiae_img
+  return minutiae_img
--- train_model.py
+++ train_model.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import pickle
 
+
 data_dir = os.getcwd() + '\\data\\train'
 sub_dir = []
 sub_file = []
@@ -12,12 +13,12 @@ for r, d, f in os.walk(data_dir):
     sub_file.append(f)
 sub_dir = sub_dir[0]
 sub_file = np.array(sub_file[1:])
-count_file = 0
-feature = []
+
+count_file = 0; feature = []
 for i in range(len(sub_file)):
     count_file += len(sub_file[i])
     for j in range(len(sub_file[i])):
-        tmp = preprocess(data_dir+'\\'+sub_dir[i]+'\\'+sub_file[i, j])
+        tmp = preprocess(data_dir+'/'+sub_dir[i]+'/'+sub_file[i, j])
         w, h = tmp.shape
         feature.append(np.reshape(tmp, w*h))
 
@@ -25,9 +26,7 @@ print(len(feature))
 
 for i in range(len(sub_dir)):
     y = np.zeros(count_file)
-    left = 0
-    right = 0
-    j = 0
+    left = 0; right = 0; j = 0
     for j in range(i):
         left += len(sub_file[j])
         right += len(sub_file[j])
